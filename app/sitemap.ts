740e0eb2799a25eecb4714b5847b025816d21b7f
Bhavishya.Verma <Bhavishya.Verma@flydocs.aero>
2025-07-12 17:49:28 +0530

    fix build error

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,23 +1,23 @@
-import { fetchAllBlogs } from "@/lib/actions/BlogsActions/BlogsActions";
+
 export const revalidate = 360;
 
 export default async function sitemap() {
-   const baseUrl = 'https://bizelevators.com';
+   const baseUrl = 'https://imyme.in';
 
-    let res = await fetchAllBlogs({pageNo: 1, pageSize: 200000});
-    let blogs = [];
-    if (res?.success) {
-        blogs = res.data || [];
-    }
+   // let res = await fetchAllBlogs({pageNo: 1, pageSize: 200000});
+    // let blogs = [];
+    // if (res?.success) {
+    //     blogs = res.data || [];
+    // }
 
-    const blogsUrls = blogs.map((blog: any) => {
-        return {
-            url: `${baseUrl}/${blog?.slug}`,
-            lastModified: new Date(),
-            changeFrequency: 'weekly',
-            priority: 1,
-        }
-    })
+    // const blogsUrls = blogs.map((blog: any) => {
+    //     return {
+    //         url: `${baseUrl}/${blog?.slug}`,
+    //         lastModified: new Date(),
+    //         changeFrequency: 'weekly',
+    //         priority: 1,
+    //     }
+    // })
 
     return [
         {
@@ -74,6 +74,6 @@ export default async function sitemap() {
             changeFrequency: 'weekly',
             priority: 1,
         },
-        ...blogsUrls,
+        // ...blogsUrls,
     ]
 }
